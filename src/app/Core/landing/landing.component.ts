b88884b71caa9d42fdb7ce4d2a74bfee57af44f4
ismaelGonzales <smile.storee@gmial.com>
2025-04-22 18:09:04 -0500

    areglos

--- src/app/Core/landing/landing.component.ts
+++ src/app/Core/landing/landing.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from "../component/navbar/navbar.component";
 import { SearchComponent } from "../component/search/search.component";
 import { FooterComponent } from "../component/footer/footer.component";
 import { CarruselComponent } from "../component/carrusel/carrusel.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [NavbarComponent, SearchComponent, FooterComponent, CarruselComponent],
+  imports: [NavbarComponent, SearchComponent, FooterComponent, CarruselComponent,RouterLink],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
